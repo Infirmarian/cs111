89e6014287aa28e84b8f995810e1d4811003a664
Robert Geil <robert@rgeil.net>
2019-02-16 13:29:13 -0800

    Updated makefile targets, and added tests for gen_csv.sh

--- lab2b/gen_csv.sh
+++ lab2b/gen_csv.sh
@@ -14,3 +14,32 @@ for thread in ${threads[@]}; do
     ./lab2_list --threads=$thread --iterations=$iterations --sync=s
 done
 
+# Generate data for multi-list implementation, make sure that all tests pass
+iterations=(1 2 4 8 16)
+threads=(1 4 8 12 16)
+lists=4
+yield_opt=id
+# No synchronization
+for it in ${iterations[@]}; do
+    for thread in ${threads[@]}; do
+        ./lab2_list --threads=$thread --iterations=$it --lists=$lists --yield=$yield_opt
+    done
+done
+
+iterations=(10 20 40 80)
+# Mutex locking
+for it in ${iterations[@]}; do
+    for thread in ${threads[@]}; do
+        ./lab2_list --threads=$thread --iterations=$it --lists=$lists --yield=$yield_opt --sync=m
+    done
+done
+# Spin locking
+for it in ${iterations[@]}; do
+    for thread in ${threads[@]}; do
+        ./lab2_list --threads=$thread --iterations=$it --lists=$lists --yield=$yield_opt --sync=s
+    done
+done
+
+
+
+
