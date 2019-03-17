12035c4e6a0149a3f31d7fce289f9bbe7709d770
Robert Geil <robert@rgeil.net>
2019-03-17 15:39:18 -0700

    Begin lab4b work

--- lab4b/lab4b.c
+++ lab4b/lab4b.c
@@ -7,6 +7,8 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #define celsius 0
 #define fahrenheit 1
@@ -14,12 +16,15 @@
 static struct option long_options[] = {
     {"period", required_argument, 0, 'p'},
     {"scale", required_argument, 0, 's'},
+    {"log", required_argument, 0, 'l'},
     {0,0,0,0}
 };
 
 int main(int argc, char** argv){
     int period = 10;
     int scale = 0;
+    char* out_file_name = 0;
+    int log_fd;
     int exit_status = 0;
     //Read in options
     int c;
@@ -59,11 +64,25 @@ int main(int argc, char** argv){
                         break;
                 }
                 break;
+            case 'l':
+                out_file_name = optarg;
+                break;
             default:
                 fprintf(stderr, "Unrecognized argument: %s", optarg);
                 exit_status = 1;
                 break;
         }
+    }
+    // Open the provided log file as output (if it exists)
+    if(out_file_name){
+        log_fd = open(out_file_name, O_WRONLY | O_APPEND);
+        if(log_fd == -1){
+            fprintf(stderr, "Unable to open the stated log file: %s", strerror(errno));
+            exit_status = 2;
+            log_fd = STDOUT_FILENO;
+        }
+    }else{
+        log_fd = STDOUT_FILENO;
     }
     return exit_status;
 }
